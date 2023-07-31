c4c2dffbce301445f4aebc58107ff49c319ca976
Jake Peterson <jdpeterson072@gmail.com>
2023-07-30 21:51:14 -0400

    Include black to white and white to black scanning

--- cv_functions.py
+++ cv_functions.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def findlines(frame,rect_start,rect_end):
+def findlines(frame,rect_start,rect_end,method='b2w'):
     if len(frame.shape) == 3:
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     else:
@@ -12,16 +12,9 @@ def findlines(frame,rect_start,rect_end):
     ret,thresh = cv2.threshold(roi,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     lsd = cv2.createLineSegmentDetector(0)
 
-    minlen = 5000
-    maxgap = 10
-    # lines = cv2.HoughLinesP(thresh,1,np.pi/180,80,minlen,maxgap)
+    minlen = 5
+
     lines = lsd.detect(thresh)[0]
-    # for x in range(0,len(lines)):
-    # cv2.imshow('test',gray)
-    # cv2.imshow('test2',roi)
-    # cv2.imshow('test3',thresh)
-    # cv2.waitKey(0)
-    # print(lines[0][0])
     if lines is not None:
         x1 = lines[0][0][0]
         y1 = lines[0][0][1]
@@ -32,20 +25,16 @@ def findlines(frame,rect_start,rect_end):
 
         line_len = int(((abs(x2-x1)**2)+(abs(y2-y1)**2)**(1/2)))
 
-        # line_left_px_above = thresh[int(y1-bw_offset),int(x1)]
-        # line_left_px_below = thresh[int(y1+bw_offset),int(x1)]
-
         line_center_px_above = thresh[int(cpy-bw_offset),int(cpx)]
         line_center_px_below = thresh[int(cpy+bw_offset),int(cpx)]
 
-        # line_right_px_above = thresh[int(y2-bw_offset),int(x2)]
-        # line_right_px_below = thresh[int(y2+bw_offset),int(x2)]
-        
-        # ave_above = (int(line_left_px_above)+int(line_center_px_above)+int(line_right_px_above))/3
-        # ave_below = (int(line_left_px_below)+int(line_center_px_below)+int(line_right_px_below))/3
-
-        if x1 != x2 and line_center_px_above < line_center_px_below and line_len > minlen:
-            return(x1,y1,x2,y2)
+        if x1 != x2  and line_len > minlen:
+            if line_center_px_above < line_center_px_below and method == "b2w":
+                return(x1,y1,x2,y2)
+            elif line_center_px_above > line_center_px_below and method == "w2b":
+                return(x1,y1,x2,y2)
+            else:
+                return(None,None,None,None)
         else:
             return(None,None,None,None)
     else:
